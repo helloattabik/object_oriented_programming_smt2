4379191b8058b3a0e7af5b1f19a0f194d49c36eb
helloattabik <attabik.1402@gmail.com>
2026-05-21 09:56:10 +0700

    add project tiket_bioskop_251240001637

---
+++ week4_constructor_in_dart/tiket_bioskop_251240001637/bin/tiket_bioskop_251240001637.dart
@@ -0,0 +1,5 @@
+import 'package:tiket_bioskop_251240001637/tiket_bioskop_251240001637.dart' as tiket_bioskop_251240001637;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tiket_bioskop_251240001637.calculate()}!');
+}
---
+++ week4_constructor_in_dart/tiket_bioskop_251240001637/test/tiket_bioskop_251240001637_test.dart
@@ -0,0 +1,8 @@
+import 'package:tiket_bioskop_251240001637/tiket_bioskop_251240001637.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
