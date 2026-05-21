35d1e4ed6f844ba68d805352f76e07a4de205749
helloattabik <attabik.1402@gmail.com>
2026-05-21 12:23:59 +0700

    add project atm_251240001637

---
+++ week5_encapsualtion/atm_2551240001637/bin/atm_2551240001637.dart
@@ -0,0 +1,5 @@
+import 'package:atm_2551240001637/atm_2551240001637.dart' as atm_2551240001637;
+
+void main(List<String> arguments) {
+  print('Hello world: ${atm_2551240001637.calculate()}!');
+}
---
+++ week5_encapsualtion/atm_2551240001637/test/atm_2551240001637_test.dart
@@ -0,0 +1,8 @@
+import 'package:atm_2551240001637/atm_2551240001637.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
