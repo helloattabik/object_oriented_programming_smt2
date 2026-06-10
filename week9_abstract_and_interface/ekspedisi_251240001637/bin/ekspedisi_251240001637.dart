925895c80d42202a66b5a7c5b0af7d37b4a9c1ba
helloattabik <attabik.1402@gmail.com>
2026-06-10 22:54:47 +0700

    add project ekspedisi_251240001637

---
+++ week9_abstract_and_interface/ekspedisi_251240001637/bin/ekspedisi_251240001637.dart
@@ -0,0 +1,5 @@
+import 'package:ekspedisi_251240001637/ekspedisi_251240001637.dart' as ekspedisi_251240001637;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ekspedisi_251240001637.calculate()}!');
+}
---
+++ week9_abstract_and_interface/ekspedisi_251240001637/test/ekspedisi_251240001637_test.dart
@@ -0,0 +1,8 @@
+import 'package:ekspedisi_251240001637/ekspedisi_251240001637.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
